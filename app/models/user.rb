93a95faa8a8cd1832acccd77b7a37a817ef45d02
mm913 <mas_m_913@yahoo.co.jp>
2022-03-06 17:58:49 +0900

    エラーメッセージ表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
-  validates :password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
+  validates :password, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
   validates :department_name,  length: { maximum: 50 }
 end

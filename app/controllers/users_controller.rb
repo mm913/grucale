1acf11bb199630f03aa0c00392a5e31f00494c5b
mm913 <mas_m_913@yahoo.co.jp>
2022-03-06 19:03:18 +0900

    editアクションの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
 
-  def index
-  end
-  
   def show
     #@user = User.find(params[:id])
     #@name = current_user.name  
     #@department_name = current_user.department_name
+  end
+
+  def edit
   end
 
 end

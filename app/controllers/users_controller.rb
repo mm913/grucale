1d0c19c937479bd0aaf0dc7ebd08910db2d92c2d
mm913 <mas_m_913@yahoo.co.jp>
2022-03-08 01:37:54 +0900

    画像の表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,9 @@ class UsersController < ApplicationController
 
   def show
     #@user = User.find(params[:id])
-    #@name = current_user.name  
-    #@department_name = current_user.department_name
   end
 
   def edit
   end
-
+  
 end

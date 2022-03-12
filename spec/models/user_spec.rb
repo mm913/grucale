e6f135651a81398eea3977758c19db4444b3d820
mm913 <mas_m_913@yahoo.co.jp>
2022-03-12 16:57:33 +0900

    単体テストコード追記

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -36,6 +36,13 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Password can't be blank")
       end
+      it "重複したemailが存在する場合は登録できない" do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
+      end
       it "passwordとpassword_confirmationが一致していないと登録できない" do
         @user.password = "aa1234"
         @user.password_confirmation = "bb1234"

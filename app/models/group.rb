fa7c1ef0922bae34941c29693adb265e971d11c1
mm913 <mas_m_913@yahoo.co.jp>
2022-03-16 22:42:21 +0900

    中間テーブル作成（再）

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,8 @@
 class Group < ApplicationRecord
   validates :group_name, presence: true, uniqueness: true, length: { minimum: 6 }
   validates :note, length: { maximum: 1000 }
+  
+  has_many :group_users
+  has_many :users, through: :gourp_users
 end
+

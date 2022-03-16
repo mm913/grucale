4ea02ddaba88d8a667f12417a3459c1a0fb7f11e
mm913 <mas_m_913@yahoo.co.jp>
2022-03-16 22:37:22 +0900

    新規作成ページの作成

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,4 @@
 class Group < ApplicationRecord
   validates :group_name, presence: true, uniqueness: true, length: { minimum: 6 }
-  validates :note length: { maximum: 1000 }
+  validates :note, length: { maximum: 1000 }
 end

f74fabd37f3b859093cdfbb572a149eda2ee028a
mm913 <mas_m_913@yahoo.co.jp>
2022-03-27 15:38:47 +0900

    newアクション、createアクション追加

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,6 +1,26 @@
 class SchedulesController < ApplicationController
 
   def index
-    #@schedule = Group.find(params[:group_id])
+    @schedules = Schedule.where(group_id: params[:group_id])
+  end
+
+  def new
+    @schedule = Schedule.new
+  end
+
+  def create
+    @schedule = Schedule.new(schedule_parameter)
+    if @schedule.save
+      @schedule = Schedule(group_id: params[:group_id])
+      redirect_to group_schedules_path(:group_id)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def schedule_parameter
+    params.require(:schedule).permit(:title, :start_time, :finish_time, :note)
   end
 end

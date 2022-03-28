df650862cc16eeb4f47314d7495558b44a39212f
mm913 <mas_m_913@yahoo.co.jp>
2022-03-28 20:45:39 +0900

    indexにグループ名表示

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -2,6 +2,7 @@ class SchedulesController < ApplicationController
 
   def index
     @schedules = Schedule.where(group_id: params[:group_id])
+    @group = Group.find(params[:group_id])
   end
 
   def new
@@ -10,13 +11,18 @@ class SchedulesController < ApplicationController
   end
 
   def create
-    Schedule.create(schedule_parameter)
-    redirect_to group_schedules_path
+    #binding.pry
+    @schedule = Schedule.new(schedule_parameter)
+    if @schedule.save
+      redirect_to group_schedules_path     
+    else
+      rendar 'new'
+    end
   end
 
   private
 
   def schedule_parameter
-    params.require(:schedule).permit(:title, :start_time, :finish_time, :note).merge(user_id: current_user.id, group_id: params[:group_id])
+    params.require(:schedule).permit(:title, :start_time, :finish_time, :all_day, :note, :user_id, :group_id)
   end
 end

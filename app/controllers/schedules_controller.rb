b86c4e80b4af63c009ada7271a440f37131a0e08
mm913 <mas_m_913@yahoo.co.jp>
2022-04-09 00:08:12 +0900

    コード微調整

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,21 +1,14 @@
 class SchedulesController < ApplicationController
   before_action :set_group, only: [:index, :create, :show, :edit, :update, :destroy]
   before_action :set_schedule, only: [:show, :edit, :update, :destroy]
+  
 
   def index
-    #@user = User.find(params[:user_id])
-    #@groups = Group.all
-    #@schedules = Group.find(params[:group_id]).schedule
     @schedules = Schedule.where(group_id: params[:group_id]).order("start_time ASC")
-    #@schedules = Schedule.all
-    @schedule = Schedule.new
-    #@schedules = @group.schedules  
+    @schedule = Schedule.new  
   end
 
-  def create
-    #binding.pry
-    #@user = User.find(params[:user_id])
-    #@schedule = Schedule.create(schedule_parameter) 
+  def create 
     @schedules = @group.schedule
     @schedule = current_user.schedules.new(schedule_parameter)
     @schedule_new = Schedule.new
@@ -58,10 +51,5 @@ class SchedulesController < ApplicationController
   def schedule_parameter
     params.require(:schedule).permit(:title, :start_time, :finish_time, :all_day, :note)
     .merge(user_id: current_user.id, group_id: params[:group_id]) 
-   # params[:group][:schedule] 
-   # if params[:group] && params[:group][:schedule]
-   #   .permit(:title, :start_time, :finish_time, :all_day, :note)
-   #   .merge(user_id: current_user.id, group_id: params[:group_id])  
-   # end
   end
 end

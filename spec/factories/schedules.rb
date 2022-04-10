8dd4b7c30af00bc3829d409cb51d430171779942
mm913 <mas_m_913@yahoo.co.jp>
2022-04-10 15:55:50 +0900

    単体テストコード

--- spec/factories/schedules.rb
+++ spec/factories/schedules.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :schedule do
-    
+    title         { "ABCD" }
+    start_time    { "2022/04/30/12/0/0" }
+    finish_time   { "2022/04/30/13/0/0" }
+    note          { Faker::Lorem.sentence }
+    association :user
+    association :group
   end
 end

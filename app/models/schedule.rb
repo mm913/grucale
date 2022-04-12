2a2792f8bfcce684243098bd42ef2ac8aab06873
mm913 <mas_m_913@yahoo.co.jp>
2022-04-12 20:47:20 +0900

    コメントモデルとテーブルの作成

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,6 +1,7 @@
 class Schedule < ApplicationRecord
   belongs_to :group
   belongs_to :user
+  has_many :comments
 
   validates :title, presence: true, length: { maximum: 24}
   validates :user_id, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_26_055814) do
+ActiveRecord::Schema.define(version: 2022_04_12_113836) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 2022_03_26_055814) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "schedule_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "group_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|

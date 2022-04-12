124f8551f698c1e9dd951234d0c6a9be7fd6dca6
mm913 <mas_m_913@yahoo.co.jp>
2022-04-12 21:29:10 +0900

    コントローラーとビューファイル作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to group_schedule_path(params[:schedule_id])
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment).merge(user_id: current_user.id, schedule_id: params[:schedule_id])
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :Schedule
+
+  validates :comment, length: { maximum: 500 }
 end
